b1148439ecc6e7333ef1c2ba2ceff9da39ddb817
B.Furkan <furkan.ibl@gmail.com>
2024-10-06 21:33:32 +0300

    Inıtial background and movement
    Inıtial commit that consist assets and basic movement

---
+++ Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+## our movement speed variable
+var movement_speed = 40.0
+
+## triggers every 1/60th of a sec, godot function
+func _physics_process(delta):
+	movement ()
+
+## custom movement function	
+func movement():
+	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left") ##right is a positive value, left is negative
+	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up") ## down is positive, up is negative, reverse of usual
+	var mov = Vector2(x_mov, y_mov) ##convert movement to vector add to movement
+	velocity = mov.normalized() * movement_speed ## multiply movement speed with normalized vector movement to get velocity
+	move_and_slide() ##godot function specificly for characterbody2d 

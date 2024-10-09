bb26351ac42082adb4873d9ed0123e88dfdb203c
B.Furkan <furkan.ibl@gmail.com>
2024-10-10 00:18:49 +0300

    added first attack (icespear)
    Added ice spear and necessary functions such as enemy detection, damage, hp, reload etc.

--- Player/player.gd
+++ Player/player.gd
@@ -4,8 +4,27 @@ extends CharacterBody2D
 var movement_speed = 40.0
 var hp = 80
 
+#attacks
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+
+#attackNodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+
+#IceSpear
+var icespear_ammo = 0
+var icespear_baseammo = 5
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+#enemy related
+var enemy_close = []
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
+
+func _ready():
+	attack()
 
 ## triggers every 1/60th of a sec, godot function
 func _physics_process(delta):
@@ -32,6 +51,47 @@ func movement():
 	velocity = mov.normalized() * movement_speed ## multiply movement speed with normalized vector movement to get velocity
 	move_and_slide() ##godot function specificly for characterbody2d 
 
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
+
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout(): ##loading ammunition
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout():
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)

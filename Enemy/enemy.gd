1236ad237d99f006d44321c880e10d447460d63b
B.Furkan <furkan.ibl@gmail.com>
2024-10-15 21:58:45 +0300

    Title screen, menu and finalization
    Added title screen, menu, song and end screen

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @export var hp = 10
 @export var knockback_recovery = 3.5
 @export var experience = 1
-@export var damage = 1
+@export var enemy_damage = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player") ##onready var gets a value after the nodes are loaded
@@ -24,7 +24,7 @@ signal remove_from_Array(object)
 
 func _ready():
 	anim.play("walk")
-	hitBox.damage = damage
+	hitBox.damage = enemy_damage
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
